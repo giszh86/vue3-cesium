1329736a3786f958103983569237e1334212bf82
c-lei-en <chenglei713@gmail.com>
2021-12-07 19:53:41 +0800

    增加别名配置

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 // 新增
 import cesium from "vite-plugin-cesium";
+
+function pathResolve(dir) {
+  return resolve(__dirname, ".", dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // 新增
   plugins: [vue(), cesium()],
+  resolve: {
+    alias: {
+      "/@": pathResolve("src"),
+    }
+  },
 });
