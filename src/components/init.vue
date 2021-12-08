2e48ca17ed1c7c059fded30d977d0dc9cd44c620
c-lei-en <chenglei713@gmail.com>
2021-12-08 20:42:29 +0800

    启用ts

--- src/components/init.vue
+++ src/components/init.vue
@@ -2,12 +2,12 @@
   <div id="cesiumContainer"></div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import CesiumNavigation from "cesium-navigation-es6";
 import { onMounted } from "vue";
 import { Viewer, Rectangle } from "cesium";
-import createImageryProvider from "@/mapConfig/addlayer/createImageryProvider";
-import addTerrain from "@/mapconfig/addTerrain/addTerrain";
+import createImageryProvider from "../mapconfig/addlayer/createImageryProvider";
+import addTerrain from "../mapconfig/addTerrain/addTerrain";
 
 onMounted(() => {
   const viewer = new Viewer("cesiumContainer", {
@@ -33,29 +33,30 @@ onMounted(() => {
     projectionPicker: false, // * 透视投影和正投影之间切换
     requestRenderMode: true, // * 在指定情况下进行渲染,提高性能
     imageryProvider: createImageryProvider(
-      "wmts",
-      "http://t0.tianditu.gov.cn/img_w/wmts?tk=c0b9cb30599dd11c468c8aaa2fc1863a",
-      {
-        layer: "img",
-        format: "image/jpeg",
-        tileMatrixSetID: "w",
-        maximumLevel: 18,
-      }
+      "arcgis",
+      "https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer"
     ),
     terrainProvider: addTerrain("ionTerrain"),
   });
-  viewer._cesiumWidget._creditContainer.style.display = "none"; // * 隐藏版权信息
-  var options = {};
-  // 用于在使用重置导航重置地图视图时设置默认视图控制。接受的值是Cesium.Cartographic 和 Cesium.Rectangle.
-  options.defaultResetView = Rectangle.fromDegrees(80, 22, 130, 50);
-  // 用于启用或禁用罗盘。true是启用罗盘，false是禁用罗盘。默认值为true。如果将选项设置为false，则罗盘将不会添加到地图中。
-  options.enableCompass = true;
-  // 用于启用或禁用缩放控件。true是启用，false是禁用。默认值为true。如果将选项设置为false，则缩放控件将不会添加到地图中。
-  options.enableZoomControls = false;
-  // 用于启用或禁用距离图例。true是启用，false是禁用。默认值为true。如果将选项设置为false，距离图例将不会添加到地图中。
-  options.enableDistanceLegend = false;
-  // 用于启用或禁用指南针外环。true是启用，false是禁用。默认值为true。如果将选项设置为false，则该环将可见但无效。
-  options.enableCompassOuterRing = true;
+
+  // * 隐藏版权信息
+  let creditContainer = <HTMLElement>viewer.cesiumWidget.creditContainer;
+  creditContainer.style.display = "none";
+
+  interface CesiumNavigationOptions {
+    defaultResetView: Rectangle; // * 用于在使用重置导航重置地图视图时设置默认视图控制。接受的值是Cesium.Cartographic 和 Cesium.Rectangle.
+    enableCompass: boolean; // * 用于启用或禁用罗盘。true是启用罗盘，false是禁用罗盘。默认值为true。如果将选项设置为false，则罗盘将不会添加到地图中。
+    enableZoomControls: boolean; // * 用于启用或禁用缩放控件。true是启用，false是禁用。默认值为true。如果将选项设置为false，则缩放控件将不会添加到地图中。
+    enableDistanceLegend: boolean; // * 用于启用或禁用比例尺。true是启用，false是禁用。默认值为true。如果将选项设置为false，比例尺将不会添加到地图中。
+    enableCompassOuterRing: boolean; // * 用于启用或禁用指南针外环。true是启用，false是禁用。默认值为true。如果将选项设置为false，则该环将可见但无效。
+  }
+  var options: CesiumNavigationOptions = {
+    defaultResetView: Rectangle.fromDegrees(80, 22, 130, 50),
+    enableCompass: true,
+    enableZoomControls: false,
+    enableDistanceLegend: false,
+    enableCompassOuterRing: true,
+  };
 
   new CesiumNavigation(viewer, options);
 });
