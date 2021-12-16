26a7a388640eb25721d5cf585bb0ba5674a8ceba
c-lei-en <chenglei713@gmail.com>
2021-12-16 19:39:35 +0800

    增加别名设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cesium()]
+  resolve: {
+    // 配置路径别名
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+    },
+  },
+  server: {
+    port: 8888,
+    // 是否自动在浏览器打开
+    open: true,
+    // 是否开启 https
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888/',
+        changeOrigin: true,
+        rewrite: (pathStr) => pathStr.replace('/api', '')
+      }
+    }
+  },
+  plugins: [vue(), cesium()],
+
 })
